3d85f4200146802c68ffe9d427d87a55dc38cd00
Tolga Ay <naezith@users.noreply.github.com>
2024-11-15 15:18:50 +0300

    Private Key Export (#183)
    * kdf show_priv_key api
    
    * display private keys
    
    * private keys QR code dialog
    
    * private keys list title
    
    * private key dialog width
    
    * private key share instead of copy
    
    * Revert "private key share instead of copy"
    
    This reverts commit a623444ea75e4d074419bcc9689585ca819beceb.
    
    * private key clipboard warning

---
+++ lib/mm2/mm2_api/rpc/show_priv_key/show_priv_key_request.dart
@@ -0,0 +1,19 @@
+import 'package:web_dex/mm2/mm2_api/rpc/base.dart';
+
+class ShowPrivKeyRequest implements BaseRequest {
+  ShowPrivKeyRequest({
+    required this.coin,
+  });
+  @override
+  late String userpass;
+  @override
+  final String method = 'show_priv_key';
+  final String coin;
+
+  @override
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'method': method,
+        'userpass': userpass,
+        'coin': coin,
+      };
+}
