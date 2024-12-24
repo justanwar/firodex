9a18f5bb1d7cab23fc03e3eadebc0724af42d82d
Francois <takenagain@users.noreply.github.com>
2024-12-24 17:24:06 +0200

    limit max_connected to 1 for electrum activation (#195)

--- lib/mm2/mm2_api/rpc/electrum/electrum_req.dart
+++ lib/mm2/mm2_api/rpc/electrum/electrum_req.dart
@@ -1,3 +1,4 @@
+import 'package:web_dex/common/screen.dart';
 import 'package:web_dex/model/electrum.dart';
 
 class ElectrumReq {
@@ -22,6 +23,12 @@ class ElectrumReq {
       'method': method,
       'coin': coin,
       'servers': servers.map((server) => server.toJson()).toList(),
+      // limit the number of active connections to electrum servers to 1 to
+      // reduce device load & request spamming. Use 3 connections on desktop
+      // and 1 on mobile (web and native) using [isMobile] until a better
+      // alternative is found
+      // https://komodoplatform.com/en/docs/komodo-defi-framework/api/legacy/coin_activation/#electrum-method
+      'max_connected': isMobile ? 1 : 3,
       'userpass': userpass,
       'mm2': mm2,
       'tx_history': true,
