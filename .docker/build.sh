ee79c1d13ec46b5c3239bb6bbdbc75a4ed122e2f
Francois <takenagain@users.noreply.github.com>
2025-06-12 19:03:00 +0200

    build: upgrade Flutter references to 3.32.2 and lower JDK target to 17 (#2759)
    * chore: bump Flutter to 3.32.2
    
    * chore(ci): restore Android build workaround and add Docker workflow
    
    * chore(docs,build): bump remaining Flutter references to `3.32.2`
    
    * build: add config-only build step to mitigate Flutter regression
    
    * chore(deps): upgrade SDK to 27683558605b58387cf22e15fa06c29ecb62edb6
    
    * build(android): revert back to JDK 17
    
    * build(docker): mount source as volume rather than including as copy step
    
    * ci(android): fix workflow dispatcher and build arg
    
    * ci(android): add temporary firebase workaround for android

--- .docker/build.sh
+++ .docker/build.sh
@@ -22,7 +22,19 @@ else
     PLATFORM_FLAG=""
 fi
 
-docker build $PLATFORM_FLAG --build-arg BUILD_USER_ID=$(id -u) -f .docker/android-sdk.dockerfile . -t komodo/android-sdk:35
+HOST_UID=$(id -u)
+HOST_GID=$(id -g)
+
+# Use fallback UID/GID if running as root to avoid conflicts
+# in GitHub Actions where the UID/GID may be 0.
+# android-sdk.dockerfile attempts to create a user with the 
+# provided UID/GID, so 0 is not a valid choice (already exists).
+if [ "$HOST_UID" = "0" ]; then
+    HOST_UID=1000
+    HOST_GID=1000
+fi
+
+docker build $PLATFORM_FLAG --build-arg BUILD_USER_ID=$HOST_UID -f .docker/android-sdk.dockerfile . -t komodo/android-sdk:35
 docker build $PLATFORM_FLAG -f .docker/komodo-wallet-android.dockerfile . -t komodo/komodo-wallet
 
 # Create the build directory ourselves to prevent it from being created by the Docker daemon (as root)
@@ -52,7 +64,8 @@ done
 # Use the provided arguments for flutter build
 # Build a second time if needed, as asset downloads will require a rebuild on the first attempt
 docker run $PLATFORM_FLAG --rm -v ./build:/app/build \
-  -u "$(id -u):$(id -g)" \
+  -v $(pwd):/app \
+  -u "$HOST_UID:$HOST_GID" \
   $ENV_ARGS \
   komodo/komodo-wallet:latest sh -c \
-  "sudo chown -R komodo:komodo /app/build; flutter pub get --enforce-lockfile && flutter build $BUILD_TARGET --no-pub --$BUILD_MODE || flutter pub get --enforce-lockfile && flutter build $BUILD_TARGET --no-pub --$BUILD_MODE"
+  "sudo chown -R komodo:komodo /app/build; flutter pub get --enforce-lockfile; flutter build web --no-pub || true; flutter build $BUILD_TARGET --config-only; flutter build $BUILD_TARGET --no-pub --$BUILD_MODE"
